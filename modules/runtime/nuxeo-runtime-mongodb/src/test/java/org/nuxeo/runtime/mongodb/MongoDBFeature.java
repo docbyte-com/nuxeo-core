f480089c2c2127eaf6ec167eee9336a92771ba96
Kevin Leturc <kevin.leturc@hyland.com>
2024-12-13 10:47:27 +0100

    NXP-32922: Don't deploy MongoDB test contrib with harness

--- modules/runtime/nuxeo-runtime-mongodb/src/test/java/org/nuxeo/runtime/mongodb/MongoDBFeature.java
+++ modules/runtime/nuxeo-runtime-mongodb/src/test/java/org/nuxeo/runtime/mongodb/MongoDBFeature.java
@@ -23,19 +23,13 @@ import static org.nuxeo.common.test.configuration.ThirdPartyUnderTest.STORAGE_MO
 import static org.nuxeo.common.test.configuration.ThirdPartyUnderTest.STORAGE_MONGODB_SERVER_PROPERTY;
 import static org.nuxeo.common.test.configuration.ThirdPartyUnderTest.STORAGE_MONGODB_SERVER_VALUE;
 
-import java.io.IOException;
-import java.net.URL;
-
 import org.nuxeo.common.test.configuration.ThirdPartyUnderTest;
-import org.nuxeo.runtime.model.URLStreamRef;
 import org.nuxeo.runtime.test.runner.ConditionalIgnoreRule;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.RunnerFeature;
 import org.nuxeo.runtime.test.runner.RuntimeFeature;
-import org.nuxeo.runtime.test.runner.RuntimeHarness;
-import org.osgi.framework.Bundle;
 
 import com.mongodb.client.MongoClient;
 
@@ -74,16 +68,6 @@ public class MongoDBFeature implements RunnerFeature {
 
     @Override
     public void start(FeaturesRunner runner) {
-        // deploy the test bundle after the default properties have been set
-        try {
-            RuntimeHarness harness = runner.getFeature(RuntimeFeature.class).getHarness();
-            Bundle bundle = harness.getOSGiAdapter().getRegistry().getBundle("org.nuxeo.runtime.mongodb.test");
-            URL url = bundle.getEntry("OSGI-INF/mongodb-test-contrib.xml");
-            harness.getContext().deploy(new URLStreamRef(url));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        // finally clear the DB
         try (MongoClient client = MongoDBConnectionHelper.newMongoClient(STORAGE_MONGODB_SERVER_VALUE)) {
             MongoDBConnectionHelper.getDatabase(client, STORAGE_MONGODB_DBNAME_VALUE).drop();
         }
